ee83eef4d703a05f62bf558b5a410d6dd21d82b0
user <user@HP>
2023-05-13 03:37:04 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус основания цилиндра");
+            double R = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите высоту цилиндра");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double V = Math.PI * (R * R) * h;
+            Console.Write("Объем цилиндра равен: " + V);
+        }
+    }
+}
